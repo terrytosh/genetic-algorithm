b84207260d8280b7e0ab3e49ca49dd1ed70e654e
Terry Tosh <tmt103@txstate.edu>
2022-10-29 03:41:30 -0500

    added some things ig

--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -2,19 +2,44 @@ import { useState } from 'react';
 
 function LeftColumn () {
 
-    const [target, setTarget] = useState("");
+    const [target, setTarget] = useState('');
 
-    const handleSubmit = (event) => {
-        return <p>{this.target}</p>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({target})
+        runAlgorithm();
+    }
+
+    const createRandomWord = () => {
+        return "random";
+    }
+
+    const runAlgorithm = () => {
+        setup();
+    }
+
+    const setup = () => {
+        var initialPopulation = [];
+        for (let i = 0; i < target.length; i++) {
+            initialPopulation[i] = createRandomWord();
+        }
+        for (let i = 0; i < target.length; i++) {
+            console.log(initialPopulation[i]);
+        }
     }
      
     return <div className="left-column">
+            <h2>Run Genetic Algorithm</h2>
             <form onSubmit={handleSubmit}>
-                <label><h2>Input a Target String:</h2>
-                <input type="text" value={target} onChange={(e) => setTarget(e.target.value)}/>
-                </label>
+                <label>Target String:</label>
+                <input
+                    type="text"
+                    required
+                    value={target}
+                    onChange={(e) => setTarget(e.target.value)}
+                />
+                <button type='submit'>Start!</button>
             </form>
-            <input type="submit" />
            </div>;
 }
 
